06733e09650a9f2f274dfecf13004a9edbc34c5c
SumonaYeasmin <SumonaYeasmin@users.noreply.github.com>
2026-07-06 14:00:00 +0600

    added about contact page design

---
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConsultationPage = () => {
+    return (
+        <div>
+            consultation page
+        </div>
+    );
+};
+
+export default ConsultationPage;
